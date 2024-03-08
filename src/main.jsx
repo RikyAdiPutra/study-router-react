609319fbe0e8ea640d16bcaa52ff283836974651
RikyAdiPutra <rikyadiputra76@gmail.com>
2024-03-08 22:00:22 +0700

    create form & createBrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Root from './routes/root.jsx'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import './index.css'
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />
+    }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 )
